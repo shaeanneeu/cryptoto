b3acc62570d0bab11795d5b6fa358b48fa839e36
shaeanneeu <shaeanneeu@gmail.com>
2025-03-16 23:15:38 +0800

    added new functions to Portfolio class

--- portfolio.py
+++ portfolio.py
@@ -6,3 +6,13 @@ from asset import Asset
 class Portfolio:
     def __init__(self, tickers: List[str], start: str, end: str):
         self.assets = {ticker: Asset(ticker, start, end) for ticker in tickers}
+    
+    def get_assets(self):
+        return self.assets
+
+    def get_asset_information(self, asset):
+        if asset in self.assets.keys():
+            asset_data = self.assets[asset].df
+            return asset_data
+        else: 
+            return f"Asset {asset} not in portfolio."
