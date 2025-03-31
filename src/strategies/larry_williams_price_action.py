ddf0d12872de7ac6f6f5ed99fb3ca3d61979194d
Valerie Tan <valerietanhx@gmail.com>
2025-03-31 14:45:36 +0800

    Add SL/TP optimisation

--- src/strategies/larry_williams_price_action.py
+++ src/strategies/larry_williams_price_action.py
@@ -1,5 +1,6 @@
 from backtesting import Strategy
 
+
 class LarryWilliamsPriceAction(Strategy):
     """
     A price action trading strategy based on
@@ -7,19 +8,35 @@ class LarryWilliamsPriceAction(Strategy):
     Larry Williams' trading strategy.
     """
 
+    size = 0.1  # Trade size
+    sl_pct = 0.02
+    tp_pct = 0.04
+
     def init(self):
         pass
 
     def next(self):
 
-        if self.data.Open[-1] > self.data.Close \
-            and self.data.High[-1] > self.data.High[-2] \
-            and self.data.Low[-1] < self.data.Low[-2] \
-            and self.data.Close[-1] < self.data.Low[-2]:
-            self.buy()
-    
-        elif self.data.Open[-1] < self.data.Close \
-            and self.data.Low[-1] < self.data.Low[-2] \
-            and self.data.High[-1] > self.data.High[-2] \
-            and self.data.Close[-1] > self.data.High[-2]:
-            self.sell()
+        curr_close = self.data.Close[-1]
+
+        if (
+            self.data.Open[-1] > self.data.Close[-1]
+            and self.data.High[-1] > self.data.High[-2]
+            and self.data.Low[-1] < self.data.Low[-2]
+            and self.data.Close[-1] < self.data.Low[-2]
+        ):
+
+            sl = curr_close - self.sl_pct * curr_close
+            tp = curr_close + self.tp_pct * curr_close
+            self.buy(size=self.size, sl=sl, tp=tp)
+
+        elif (
+            self.data.Open[-1] < self.data.Close[-1]
+            and self.data.Low[-1] < self.data.Low[-2]
+            and self.data.High[-1] > self.data.High[-2]
+            and self.data.Close[-1] > self.data.High[-2]
+        ):
+
+            sl = curr_close + self.sl_pct * curr_close
+            tp = curr_close - self.tp_pct * curr_close
+            self.sell(size=self.size, sl=sl, tp=tp)
