0a4f9e6b128abc191a4e5d52cb024f60c8bfd87a
shotnothing <shotnothing@protonmail.ch>
2025-03-31 15:36:17 +0800

    Add volume spike reversal strategy

--- src/strategies/volume_spike_reversal.py
+++ src/strategies/volume_spike_reversal.py
@@ -1,49 +1,27 @@
-import pandas as pd
-
-from models.signals import HOLD, LONG, SHORT
-from models.strategy import Strategy
-
+from backtesting import Strategy
 
 class VolumeSpikeReversal(Strategy):
-    def generate_signals(self, df: pd.DataFrame) -> pd.DataFrame:
-        """
-        Volume Spike Reversal Strategy:
-        - LONG: Volume spike + bullish candle + recent downtrend
-        - SHORT: Volume spike + bearish candle + recent uptrend
-
-        Parameters:
-            df (pd.DataFrame): Asset's historical OHLCV data.
-
-        Returns:
-            pd.DataFrame: DataFrame with 'TotalSignal' column.
-        """
-
-        df["AvgVolume10"] = df["Volume"].rolling(window=10).mean()
-
-        def is_downtrend(close_series):
-            return close_series[-1] < close_series[0]
-
-        def is_uptrend(close_series):
-            return close_series[-1] > close_series[0]
-
-        def total_signal(df: pd.DataFrame, curr):
-            pos = df.index.get_loc(curr)
-            if pos < 11:
-                return HOLD
-
-            row = df.iloc[pos]
-            prev_closes = df["Close"].iloc[pos - 10 : pos]
-
-            volume_spike = row["Volume"] > 2 * df["AvgVolume10"].iloc[pos]
-
-            if volume_spike:
-                if row["Close"] > row["Open"] and is_downtrend(prev_closes):
-                    return LONG
-                if row["Close"] < row["Open"] and is_uptrend(prev_closes):
-                    return SHORT
-
-            return HOLD
-
-        df["TotalSignal"] = df.apply(lambda row: total_signal(df, row.name), axis=1)
-
-        return df
+    """
+    Volume Spike Reversal Strategy:
+    - LONG: Volume spike + bullish candle + recent downtrend
+    - SHORT: Volume spike + bearish candle + recent uptrend
+    """
+    def init(self):
+        pass
+    
+    def next(self):
+        if len(self.data.Close) < 10:
+            return
+        
+        sma10_volume = self.data.Volume[-10:].mean()
+        
+        if self.data.Volume[-1] <= 2 * sma10_volume:
+            return
+        
+        if self.data.Close[-1] < self.data.Open[-1] \
+            and self.data.Close[-2] > self.data.Close[-1]:
+            self.sell()
+        
+        if self.data.Close[-1] > self.data.Open[-1] \
+            and self.data.Close[-2] < self.data.Close[-1]:
+            self.buy()
