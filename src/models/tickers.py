60013f6e551a2afcdc1f13c5226872fde1194bec
Valerie Tan <valerietanhx@gmail.com>
2025-04-03 12:10:51 +0800

    Edit autotrade s.t. we no longer need to manually edit asset_strategies.csv

--- src/models/tickers.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-tickers = ["NVDA", "MSFT"] # TBC
-proportions = [0.5, 0.5] # TBC
-
-allocation = pd.DataFrame({"Ticker": tickers, "Proportion": proportions})
-# to adjust this file based on max_sharpe_allocation.csv once finalised
